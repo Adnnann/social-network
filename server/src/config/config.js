5662098f3c0fc090cffdfda084c3b22fca9bc16e
Adnan Ovcina <adnanovcina1987@gmail.com>
2023-02-09 10:14:46 +0100

    Update config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = {
   port: process.env.PORT || 4400,
   secret: process.env.JWT_SECRET || "Your secret key",
-  mongoUri: process.env.MONGO || "mongodb://localhost:27017/mernproject",
+  mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/mernproject",
 };
 
 module.exports = config;
